def33ddf071a37b7afd1ce3a38114a186ee28bc8
raqueelima <raqueelima1@gmail.com>
2024-07-21 19:48:47 -0300

    Initial commit

---
+++ Livro.java
@@ -0,0 +1,22 @@
+package BibliotecaXPTO;
+
+public class Livro extends Publicacao{
+    private int isbn;
+    private int edicao;
+    
+
+    public Livro(String titulo , String autor , int ano , String genero , String editora , int qntd, int isbn, int edicao){
+        super(titulo, autor, ano, genero, editora, qntd);
+        this.isbn = isbn;
+        this.edicao = edicao;
+    }
+
+    public int getIsbn(){
+        return isbn;
+    }
+
+    public int getEdicao(){
+        return edicao;
+    }
+    
+}
